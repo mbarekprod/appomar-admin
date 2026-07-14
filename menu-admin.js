f1e5da3ab739c20ba6d72ddf575fa76fb0c69d5a
mbarekprod <mbarekprod1@gmail.com>
2026-07-15 00:56:16 +0300

    Implement addCategory function for categories
    Added functionality to add a new category to Firestore.

--- menu-admin.js
+++ menu-admin.js
@@ -1,1 +1,38 @@
+import { db } from "./firebase.js";
 
+import {
+    collection,
+    addDoc,
+    deleteDoc,
+    updateDoc,
+    doc,
+    onSnapshot,
+    query,
+    orderBy
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+
+const categoriesRef = collection(db, "categories");
+const productsRef = collection(db, "products");
+
+window.addCategory = async function () {
+
+    const input = document.getElementById("categoryName");
+
+    const name = input.value.trim();
+
+    if (!name) {
+        alert("أدخل اسم الصنف");
+        return;
+    }
+
+    await addDoc(categoriesRef, {
+
+        name,
+
+        createdAt: Date.now()
+
+    });
+
+    input.value = "";
+
+};
